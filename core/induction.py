f9a5f41f981c75c014c41555fe275cc158412175
ZhengyaoJiang <jzyjiangzhengyao@gmail.com>
2018-04-07 13:29:50 +0100

    finish clauses generation

---
+++ core/induction.py
@@ -0,0 +1,5 @@
+from __future__ import print_function, division, absolute_import
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+
--- core/rules.py
+++ core/rules.py
@@ -19,16 +19,37 @@ class RulesManager():
         else:
             predicates = [self.__language.extensional]
         terms = []
-        print(predicates)
         for predicate in predicates:
-            print(predicate)
             body_variables = [body_variable for _ in range(predicate.arity)]
-            terms += self.generate_term_atoms(predicate, *body_variables)
+            terms += self.generate_body_atoms(predicate, *body_variables)
         result_tuples = product(head, terms, terms)
-        return [Clause(result[0], result[1:]) for result in result_tuples]
+        return self.prune([Clause(result[0], result[1:]) for result in result_tuples])
 
     @staticmethod
-    def generate_term_atoms(predicate, *variables):
+    def prune(clauses):
+        pruned = []
+        def not_unsafe(clause):
+            head_variables = set(clause.head.terms)
+            body_variables = set(clause.body[0].terms+clause.body[1].terms)
+            return head_variables.issubset(body_variables)
+
+        def not_circular(clause):
+            return clause.head not in clause.body
+
+        def not_duplicated(clause):
+            for pruned_caluse in pruned:
+                if reversed(pruned_caluse.body) == clause.body:
+                    return False
+            return True
+
+        for clause in clauses:
+            if not_unsafe(clause) and not_circular(clause) and not_duplicated(clause):
+                pruned.append(clause)
+        return pruned
+
+
+    @staticmethod
+    def generate_body_atoms(predicate, *variables):
         '''
         :param predict_candidate: string, candiate of predicate
         :param variables: iterable of tuples of integers, candidates of variables at each position

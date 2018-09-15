986ed5db63da9fc432e664985917f5f38fa15f2d
Rooftrellen <jzyjiangzhengyao@gmail.com>
2018-09-15 16:12:19 +0800

    add auto string conversion and varible dynamic binding

--- core/clause.py
+++ core/clause.py
@@ -4,6 +4,54 @@ from collections import namedtuple
 
 Predicate = namedtuple("Predicate", "name arity")
 
+
+def is_variable(term):
+    return True if isinstance(term, int) else False
+
+def var_string(atom):
+    '''
+    find all variable string (string with first letter capitalized)
+    :param atom: atom where variables not replaced with integers
+    :return: set of variables string
+    '''
+    vars = set()
+    for term in atom.terms:
+        if term[0].isupper():
+            vars.add(term)
+    return vars
+
+def str2atom(s):
+    '''
+    :param s: 
+    :return: Atom where variables not replaced with integers
+    '''
+    s = s.replace(" ", "")
+    left = s.find("(")
+    right = s.find(")")
+    terms = s[left + 1:right].split(",")
+    predicate = Predicate(s[:left], len(terms))
+    return Atom(predicate, terms)
+
+def str2clause(s):
+    """
+    :param s: 
+    :return: 
+    """
+    s = s.replace(" ", "")
+    atoms = s.split(":-")
+    head_str = atoms[0]
+    body_strs = atoms[1].replace("),",") ").split(" ")
+    head = str2atom(head_str)
+    body = [str2atom(s) for s in body_strs]
+    clause = Clause(head, body)
+    var_strs = set()
+    for atom in body+[head]:
+        print(atom)
+    for strs in [var_string(atom) for atom in body+[head]]:
+        var_strs = var_strs.union(strs)
+    return clause.replace_by_dict({s: i for i,s in enumerate(var_strs)})
+
+
 class Atom(object):
     def __init__(self, predicate, terms):
         '''
@@ -26,13 +74,11 @@ class Atom(object):
         return hash(tuple(hashed_list))
 
     def __eq__(self, other):
-        """Overrides the default implementation"""
         if isinstance(self, other.__class__):
             return self.__dict__ == other.__dict__
         return False
 
     def __ne__(self, other):
-        """Overrides the default implementation (unnecessary in Python 3)"""
         return not self.__eq__(other)
 
     def __str__(self):
@@ -69,7 +115,7 @@ class Atom(object):
                 match[self.terms[i]] = target.terms[i]
         return match
 
-    def replace_variable(self, match):
+    def replace(self, match):
         '''
         :param match: match dictionary
         :return: a atoms whose variable is replaced by constants, given the match mapping.
@@ -82,6 +128,13 @@ class Atom(object):
                 terms.append(match[variable])
         result = Atom(self.predicate, terms)
         return result
+
+    def assign_var_id(self, start):
+        var_map = {}
+        for term in self.terms:
+            if isinstance(term, int):
+                var_map[term] = start+term
+        return self.replace(var_map)
 
 
 class Clause():
@@ -109,8 +162,13 @@ class Clause():
         match = self.head.match_variable(head)
         new_body = []
         for atom in self.body:
-            new_body.append(atom.replace_variable(match))
+            new_body.append(atom.replace(match))
         return Clause(head, new_body)
+
+    def replace_by_dict(self, match):
+        head = self.head.replace(match)
+        body = [atom.replace(match) for atom in self.body]
+        return Clause(head, body)
 
     def __hash__(self):
         hashed_list = list(self.body[:])
@@ -118,12 +176,15 @@ class Clause():
         return hash(tuple(hashed_list))
 
     def __eq__(self, other):
-        """Overrides the default implementation"""
         if isinstance(self, other.__class__):
             return self.__dict__ == other.__dict__
         return False
 
     def __ne__(self, other):
-        """Overrides the default implementation (unnecessary in Python 3)"""
         return not self.__eq__(other)
+
+    def assign_var_id(self, start):
+        head = self.head.assign_var_id(start)
+        body = [atom.assign_var_id(start) for atom in self.body]
+        return Clause(head, body)
 

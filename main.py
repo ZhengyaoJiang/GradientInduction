f4a898e99756a37f6ba19872f87efba3eaf95e99
Zhengyao Jiang <jzyjiangzhengyao@gmail.com>
2018-09-23 17:51:24 +0100

    fix bug in the problem definition

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     constants = [str(i) for i in range(10)]
     background = [Atom(Predicate("succ", 2), [constants[i], constants[i + 1]]) for i in range(9)]
     background.append(Atom(Predicate("zero", 1), "0"))
-    positive = [Atom(Predicate("predecessor", 2), [constants[i + 1], constants[i]]) for i in range(9)]
+    positive = [Atom(Predicate("predecessor", 2), [constants[i + 2], constants[i]]) for i in range(8)]
     all_atom = [Atom(Predicate("predecessor", 2), [constants[i], constants[j]]) for i in range(9) for j in range(9)]
     negative = list(set(all_atom) - set(positive))
 
     language = LanguageFrame(Predicate("predecessor",2), [Predicate("zero",1), Predicate("succ",2)], constants)
     ilp = ILP(language, background, positive, negative)
     program_temp = ProgramTemplate([], {Predicate("predecessor", 2): [RuleTemplate(1, True), RuleTemplate(1, True)]},
-                                   10)
+                                   4)
     man = RulesManager(language, program_temp)
 
     # clauses = man.generate_clauses(Predicate("predecessor", 2), RuleTemplate(1, True))
